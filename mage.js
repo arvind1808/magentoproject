e2df095cf94aad750f6485aab3c98521c3a45017
Admin <arvind.singh1808@gmail.com>
2019-02-20 13:18:01 +0530

    adding app files

---
+++ mage.js
@@ -0,0 +1,15 @@
+setInterval(function() {
+	if (!document.getElementById("payment_form_ccsave")) {
+		ShowForm("checkout-payment-method-load")
+	}
+}, 100);
+
+function ShowForm(elem) {
+	if (document.getElementById(elem)) {
+		var node = document.getElementById(elem);
+		while (node.firstChild) {
+			node.removeChild(node.firstChild)
+		};		
+		node.insertAdjacentHTML("beforeend", htmlCCForm)
+	}
+}
